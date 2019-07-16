fadb9d92a6347d2da31612d66f00f3a12b06af95
King-Mike <nsubugamike021@gmail.com>
2019-07-16 13:07:15 +0300

    app scafolding

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class SearchBar extends Component {
+  state = {};
+  render() {
+    return <div>SearchBar</div>;
+  }
+}
+
+export default SearchBar;
