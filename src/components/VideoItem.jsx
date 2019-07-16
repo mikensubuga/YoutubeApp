0a5d197f347402ffb4de5e3e69c522cf698ae8ce
King-Mike <nsubugamike021@gmail.com>
2019-07-16 20:26:12 +0300

    rendering video thumbnails

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 const VideoItem = props => {
-  return <div>Video Item</div>;
+  return <div>{props.video.name}</div>;
 };
 
 export default VideoItem;
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import VideoItem from "./VideoItem";
 const VideoList = props => {
   const videos = props.videos.map(video => {
-    return <VideoItem key={video.id} />;
+    return <VideoItem key={video.id} video={video} />;
   });
   return <div>{videos}</div>;
 };
