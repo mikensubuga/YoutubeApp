a1a704938361db76e307cebe9236b99c81961e58
King-Mike <nsubugamike021@gmail.com>
2019-07-16 23:24:34 +0300

    styling video detail

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,7 +4,14 @@ const VideoDetail = props => {
   if (!props.video) {
     return "Loading...";
   }
-  return <div>{props.video.name}</div>;
+  return (
+    <div>
+      <div className="ui segment">
+        <h4 className="ui header"> {props.video.name}</h4>
+        <p>{props.video.detail}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
