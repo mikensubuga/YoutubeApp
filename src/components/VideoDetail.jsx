0b38786ae75076ac678f9e03ba120f39c171cc33
King-Mike <nsubugamike021@gmail.com>
2019-07-16 23:36:47 +0300

    displaying a video player

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,8 +4,12 @@ const VideoDetail = props => {
   if (!props.video) {
     return "Loading...";
   }
+  const videoSrc = `https://www.youtube.com/embed/${props.video.id}`;
   return (
     <div>
+      <div className="ui embed">
+        <iframe src="" />
+      </div>
       <div className="ui segment">
         <h4 className="ui header"> {props.video.name}</h4>
         <p>{props.video.detail}</p>
